a401b7531ebdcf154761ea280bc2453746f0bd12
Eduardo Sá <eduardoxaviersa@gmail.com>
2026-01-24 00:38:32 +0000

    .NET MAUI GUI Project Setup / Connection with C++ engine (through C# Wrapper).

---
+++ SDLab_InteropWrapper/AudioEngineWrapper.cs
@@ -0,0 +1,49 @@
+﻿using AUDIOPROCBRIDGE;
+
+namespace SDLab_InteropWrapper
+{
+    public class AudioEngineWrapper
+    {
+        AudioEngineRef audioEngineRef;
+
+        public AudioEngineWrapper()
+        {
+            audioEngineRef = new AudioEngineRef();
+        }
+
+        public int test()
+        {
+            return 3301;
+        }
+
+        public IntPtr CreateEngine()
+        {
+            return audioEngineRef._createEngine();
+        }
+
+        public void EnginePrepareToPlay(IntPtr engine, double sampleRate, int samplesPerBlockExpected)
+        {
+            audioEngineRef._enginePrepareToPlay(engine, sampleRate, samplesPerBlockExpected);
+        }
+
+        public void EngineProcessWave(IntPtr engine, float[] buffer, int numSamples, int offset)
+        {
+            audioEngineRef._engineProcessWave(engine, buffer, numSamples, offset);
+        }
+
+        public void DestroyEngine(IntPtr engine)
+        {
+            audioEngineRef._destroyEngine(engine);
+        }
+
+        public void ChangeFrequency(IntPtr engine, float newFrequency)
+        {
+            audioEngineRef._changeFrequency(engine, newFrequency);
+        }
+
+        public void ChangeGain(IntPtr engine, float newGain)
+        {
+            audioEngineRef._changeGain(engine, newGain);
+        }
+    }
+}
---
+++ SpectralDynaLab_GUI/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SDLab_InteropWrapper;
+
+namespace SpectralDynaLab_GUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private AudioEngineMGMT audioManager;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AudioEngineWrapper audioEngineWrapper = new AudioEngineWrapper();
+            audioManager = new AudioEngineMGMT();
+
+            JuceAudioProvider provider2 = audioManager.LaunchAudioEngine();
+
+            audioManager.PlayMixer();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
