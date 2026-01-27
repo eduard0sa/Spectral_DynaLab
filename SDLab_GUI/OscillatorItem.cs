33fe72203d7bd6f8e66a0f188fecdde234c8d977
eduard0sa <eduardoxaviersa@gmail.com>
2026-01-27 12:55:47 +0000

    Working on dynamic oscillator item.

---
+++ SDLab_GUI/OscillatorItem.cs
@@ -0,0 +1,32 @@
+﻿namespace SDLab_GUI
+{
+    class OscillatorItem : FlexLayout
+    {
+        FlexLayout OscillatorTriangleMark = new FlexLayout();
+        FlexLayout OscillatorItemHeader = new FlexLayout();
+        FlexLayout OscillatorItemSliderControls = new FlexLayout();
+
+        OscillatorItem()
+        {
+            HorizontalOptions = LayoutOptions.Fill;
+            Direction = Microsoft.Maui.Layouts.FlexDirection.Row;
+            JustifyContent = Microsoft.Maui.Layouts.FlexJustify.SpaceBetween;
+            HeightRequest = 100;
+            BackgroundColor = Color.FromHex("#14141d");
+
+            //FlexLayout.Grow="0.05" JustifyContent="Center" AlignItems="Center"
+            OscillatorTriangleMark.JustifyContent = Microsoft.Maui.Layouts.FlexJustify.Center;
+            OscillatorTriangleMark.AlignItems = Microsoft.Maui.Layouts.FlexAlignItems.Center;
+
+            //FlexLayout.Grow="0.16" JustifyContent="Center" AlignItems="Center"
+            OscillatorItemHeader.JustifyContent = Microsoft.Maui.Layouts.FlexJustify.Center;
+            OscillatorItemHeader.AlignItems = Microsoft.Maui.Layouts.FlexAlignItems.Center;
+
+            //FlexLayout.Grow="0.16" Direction="Column" JustifyContent="Center" AlignItems="Center" VerticalOptions="Fill"
+            OscillatorItemSliderControls.Direction = Microsoft.Maui.Layouts.FlexDirection.Column;
+            OscillatorItemSliderControls.JustifyContent = Microsoft.Maui.Layouts.FlexJustify.Center;
+            OscillatorItemSliderControls.AlignItems = Microsoft.Maui.Layouts.FlexAlignItems.Center;
+            OscillatorItemSliderControls.VerticalOptions = LayoutOptions.Fill;
+        }
+    }
+}
